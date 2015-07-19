610680e870eaedb34e30f75b400273cc77dea066
viktorstrate <viktorstrate@gmail.com>
2015-07-19 17:36:44 +0200

    Got it back

--- src/minecraftDownloader.js
+++ src/minecraftDownloader.js
@@ -14,3 +14,27 @@ function getServerVersionList(callback) {
 function getServerVersionUrl(version) {
     return minecraftServerUrl + version + '/minecraft_server.' + version + '.jar';
 }
+
+function downloadServer(version, path) {
+    deferred = $.Deferred();
+
+    var file = fs.createWriteStream(path + '/minecraft_server.jar');
+
+    http.get(getServerVersionUrl(version), function (response) {
+        var length = response.headers['content-length'];
+        console.log(length);
+        deferred.notify({length: length});
+
+        response.on('data', function (chunk) {
+            console.log('got %d bytes of data out of %d', chunk.length, length);
+            deferred.notify({progress: chunk.length});
+        });
+        response.pipe(file);
+    }).on('error', function (e) {
+        deferred.reject(e.message);
+    }).on('close', function () {
+        deferred.resolve();
+    });
+
+    return deferred.promise();
+}
